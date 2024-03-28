c72c08e4c3c222d0434d121d8245a61df358ac10
isosafrasaurus <56939070+isosafrasaurus@users.noreply.github.com>
2024-03-28 12:13:31 -0500

    helpers for graph

--- vertex.v
+++ vertex.v
@@ -1,5 +1,7 @@
 Require Import Coq.Arith.Arith.
 Require Import Coq.Bool.Bool.
+Require Import Coq.Lists.List.
+Import ListNotations.
 
 (* Define a vertex wrapper around nat. *)
 Structure V := mkV { unV : nat }.
@@ -17,3 +19,12 @@ Proof.
   - left; reflexivity.
   - right; reflexivity.
 Qed.
+
+Fixpoint find_index (v : V) (vs : list V) (current_index : nat) : nat :=
+  match vs with
+  | [] => current_index
+  | h :: t => if (unV v =? unV h) then current_index else find_index v t (S current_index)
+  end.
+
+Definition nth_bool (l : list bool) (n : nat) : bool :=
+  nth n l false.
